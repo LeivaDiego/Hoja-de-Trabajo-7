a72113664395dec291b14608715cc9e2eeb46a0a
LeivaDiego <lei21752@uvg.edu.gt>
2023-03-25 21:15:33 -0600

    Version 5.0 FileManager
    Clase manejadora de archivos de texto
    Pequeñas modificaciones en el UML, se agregaron nuevos parametros a los metodos de la clase FileManager, y nuevas relaciones entre la clase View y FileManager

--- BST/src/Main.java
+++ BST/src/Main.java
@@ -1,5 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        String filePath = "C:/Users/diego/Documents/UVG/5to Semestre/Algoritmos y Estructuras de Datos/Hoja-de-Trabajo-7/BST/src/texto.txt";
+        String dictPath = "C:/Users/diego/Documents/UVG/5to Semestre/Algoritmos y Estructuras de Datos/Hoja-de-Trabajo-7/BST/src/diccionario.txt";
+
         System.out.println("Hello world!");
 
     }
